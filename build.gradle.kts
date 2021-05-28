cd493d4dedbbd658de571e12b8783788179e8bb5
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-05-28 21:09:32 +0200

    Update build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,19 +24,16 @@ description = "Discord utilities for use with JDA"
 java.sourceCompatibility = JavaVersion.VERSION_15
 
 publishing {
+    val publishData = PublishData(project)
     publications.create<MavenPublication>("maven") {
         from(components["java"])
         groupId = project.group as String?
         artifactId = project.name
-        version = project.version as String?
+        version = publishData.getVersion()
     }
 
     repositories {
         maven {
-            val isSnapshot = version.toString().endsWith("SNAPSHOT");
-            val release = "https://eldonexus.de/repository/maven-releases/";
-            val snapshot = "https://eldonexus.de/repository/maven-snapshots/";
-
             authentication {
                 credentials(PasswordCredentials::class) {
                     username = System.getenv("NEXUS_USERNAME")
@@ -45,7 +42,7 @@ publishing {
             }
 
             name = "EldoNexus"
-            url = uri(if (isSnapshot) snapshot else release)
+            url = uri(publishData.getRepository())
         }
     }
 }
@@ -60,5 +57,41 @@ tasks.test {
     useJUnitPlatform()
     testLogging {
         events("passed", "skipped", "failed")
+    }
+}
+
+class PublishData(private val project: Project) {
+    var type: Type = getReleaseType()
+    var hashLength: Int = 7
+
+    private fun getReleaseType(): Type {
+        val branch = getCheckedOutBranch()
+        return when {
+            branch.contentEquals("master") -> Type.RELEASE
+            branch.startsWith("dev") -> Type.DEV
+            else -> Type.SNAPSHOT
+        }
+    }
+
+    private fun getCheckedOutGitCommitHash(): String = System.getenv("GITHUB_SHA")?.substring(0, hashLength) ?: "local"
+
+    private fun getCheckedOutBranch(): String = System.getenv("GITHUB_REF")?.replace("refs/heads/", "") ?: "local"
+
+    fun getVersion(): String = getVersion(false)
+
+    fun getVersion(appendCommit: Boolean): String =
+        type.append(getVersionString(), appendCommit, getCheckedOutGitCommitHash())
+
+    private fun getVersionString(): String = (project.version as String).replace("-SNAPSHOT", "").replace("-DEV", "")
+
+    fun getRepository(): String = type.repo
+
+    enum class Type(private val append: String, val repo: String, private val addCommit: Boolean) {
+        RELEASE("", "https://eldonexus.de/repository/maven-releases/", false),
+        DEV("-DEV", "https://eldonexus.de/repository/maven-dev/", true),
+        SNAPSHOT("-SNAPSHOT", "https://eldonexus.de/repository/maven-snapshots/", true);
+
+        fun append(name: String, appendCommit: Boolean, commitHash: String): String =
+            name.plus(append).plus(if (appendCommit && addCommit) "-".plus(commitHash) else "")
     }
 }
