8ba136e2f262e65aead36b518d52c6211150a266
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-06-19 00:47:30 +0200

    Move data stuff to sql util

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
 }
 
 group = "de.chojo"
-version = "1.4.1"
+version = "1.5.0"
 description = "Discord utilities for use with JDA"
 java.sourceCompatibility = JavaVersion.VERSION_15
 
