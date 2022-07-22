2ac0b2b1aa7d2dc7ecdf8c20c3bdf2639bda369a
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-07-22 18:07:25 +0200

    Bump version to 2.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 }
 
 dependencies {
-    api("net.dv8tion", "JDA", "5.0.0-alpha.16")
+    api("net.dv8tion", "JDA", "5.0.0-alpha.17")
     api("org.apache.commons", "commons-text", "1.9")
     api("club.minnced", "discord-webhooks", "0.8.2")
 
@@ -38,7 +38,7 @@ license {
 }
 
 group = "de.chojo"
-version = "2.6.2+alpha.15"
+version = "2.7.0+alpha.17"
 description = "Discord utilities for use with JDA"
 
 publishData {
