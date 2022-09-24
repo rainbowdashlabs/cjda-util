6cdb9930b9769a2bcbabe0b3673ac95d448e9683
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-09-24 12:00:56 +0200

    Fix command groups not recognized

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,8 @@ dependencies {
 
     // unit testing
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter", "junit-jupiter")
+    testImplementation("org.mockito", "mockito-core", "4.5.1")
 }
 
 license {
