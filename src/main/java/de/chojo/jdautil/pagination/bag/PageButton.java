442509f839ea58460042e9cf4a660170ef1732ad
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-07-06 09:16:55 +0200

    Add factory method for page buttons

--- src/main/java/de/chojo/jdautil/pagination/bag/PageButton.java
+++ src/main/java/de/chojo/jdautil/pagination/bag/PageButton.java
@@ -9,8 +9,25 @@ package de.chojo.jdautil.pagination.bag;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.interactions.components.buttons.ButtonInteraction;
 
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
 public interface PageButton {
     Button button(IPageBag page);
 
     void invoke(IPageBag pageBag, ButtonInteraction event);
+
+    static PageButton of(Function<IPageBag, Button> button, BiConsumer<IPageBag, ButtonInteraction> consumer) {
+        return new PageButton() {
+            @Override
+            public Button button(IPageBag page) {
+                return button.apply(page);
+            }
+
+            @Override
+            public void invoke(IPageBag pageBag, ButtonInteraction event) {
+                consumer.accept(pageBag, event);
+            }
+        };
+    }
 }
