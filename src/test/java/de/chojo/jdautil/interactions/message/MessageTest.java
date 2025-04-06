26672965b7cfb14fcadf1d418f272b18284ba60e
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2025-04-06 18:57:50 +0200

    Fix compile error

--- src/test/java/de/chojo/jdautil/interactions/message/MessageTest.java
+++ src/test/java/de/chojo/jdautil/interactions/message/MessageTest.java
@@ -9,7 +9,6 @@ package de.chojo.jdautil.interactions.message;
 public class MessageTest {
     void test(){
         Message.of("test")
-                .handler(null)
-                .setContext();
+                .handler(null);
     }
 }
