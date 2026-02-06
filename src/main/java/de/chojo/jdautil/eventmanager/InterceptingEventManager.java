9ee8fef3ba2120709a02c12db7a60d1144a35d15
Nora <46890129+RainbowDashLabs@users.noreply.github.com>
2026-02-06 14:22:42 +0100

    add license

--- src/main/java/de/chojo/jdautil/eventmanager/InterceptingEventManager.java
+++ src/main/java/de/chojo/jdautil/eventmanager/InterceptingEventManager.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) RainbowDashLabs and Contributor
+ */
+
 package de.chojo.jdautil.eventmanager;
 
 import net.dv8tion.jda.api.events.GenericEvent;
